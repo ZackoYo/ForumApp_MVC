5083135580e40a555f8140674469b3101d177364
ZackoYo <zarko.y@gmail.com>
2024-10-09 19:23:22 +0300

    Add Post service and create model

---
+++ src/Services/ForumAppMVC.Services.Data/PostsService.cs
@@ -0,0 +1,31 @@
+﻿using ForumAppMVC.Data.Common.Repositories;
+using ForumAppMVC.Data.Models;
+using System.Threading.Tasks;
+
+namespace ForumAppMVC.Services.Data
+{
+    public class PostsService : IPostsService
+    {
+        private readonly IDeletableEntityRepository<Post> postsRepo;
+        public PostsService(IDeletableEntityRepository<Post> postsRepo)
+        {
+            this.postsRepo = postsRepo;
+        }
+
+        public async Task<int> CreateAsync(string title, string content, int categoryId, string userId)
+        {
+            var post = new Post
+            {
+                CategoryId = categoryId,
+                Content = content,
+                Title = title,
+                UserId = userId,
+            };
+
+            await this.postsRepo.AddAsync(post);
+            await this.postsRepo.SaveChangesAsync();
+
+            return post.Id;
+        }
+    }
+}
---
+++ src/Web/ForumAppMVC.Web.ViewModels/Posts/PostCreateInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForumAppMVC.Web.ViewModels.Posts
+{
+    public class PostCreateInputModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Range(1, int.MaxValue)]
+        [Display(Name = "Category")]
+
+        public int CategoryId { get; set; }
+    }
+}
