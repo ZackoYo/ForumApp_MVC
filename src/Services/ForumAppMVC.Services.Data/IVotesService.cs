93d9231fe97e2efd756e026c3ff9a9105cd7ec1f
ZackoYo <zarko.y@gmail.com>
2024-10-23 23:56:18 +0300

    Add Votes Service interface

---
+++ src/Services/ForumAppMVC.Services.Data/IVotesService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace ForumAppMVC.Services.Data
+{
+    public interface IVotesService
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="postId"></param>
+        /// <param name="userId"></param>
+        /// <param name="isUpVote">If true - up vote, else - down vote.</param>
+        /// <returns></returns>
+        Task VoteAsync(int postId, string userId, bool isUpVote);
+    }
+}
