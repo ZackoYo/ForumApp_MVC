906b398c33fcccce7e931564eaa9c1e57ebe08b7
ZackoYo <zarko.y@gmail.com>
2024-10-25 00:10:05 +0300

    Create VotesCotroller

---
+++ src/Web/ForumAppMVC.Web/Controllers/VotesController.cs
@@ -0,0 +1,24 @@
+﻿using ForumAppMVC.Services.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumAppMVC.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VotesController : ControllerBase
+    {
+        private readonly IVotesService votesService;
+        public VotesController(
+            IVotesService votesService)
+        {
+            this.votesService = votesService;
+        }
+
+        [HttpPost]
+        public ActionResult Post()
+        {
+            //TODO
+            return this.Ok();
+        }
+    }
+}
