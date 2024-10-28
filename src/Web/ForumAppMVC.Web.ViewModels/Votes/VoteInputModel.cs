107beb8eefde772e7bceb9c427670683b2e24abb
ZackoYo <zarko.y@gmail.com>
2024-10-28 22:10:43 +0200

    Add PostVote models

---
+++ src/Web/ForumAppMVC.Web.ViewModels/Votes/VoteInputModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ForumAppMVC.Web.ViewModels.Votes
+{
+    public class VoteInputModel
+    {
+        public int PostId { get; set; }
+        public bool IsUpVote { get; set; }
+    }
+}
---
+++ src/Web/ForumAppMVC.Web.ViewModels/Votes/VoteResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ForumAppMVC.Web.ViewModels.Votes
+{
+    public class VoteResponseModel
+    {
+        public int VotesCount { get; set; }
+    }
+}
