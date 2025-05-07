1c1bd09117d609f538a84744310d27532201d503
ZackoYo <zarko.y@gmail.com>
2025-05-07 10:14:34 +0300

    Add Create comment endpoint

--- src/Web/ForumAppMVC.Web/Controllers/CommentsController.cs
+++ src/Web/ForumAppMVC.Web/Controllers/CommentsController.cs
@@ -1,14 +1,31 @@
 ﻿using System.Threading.Tasks;
+using ForumAppMVC.Data.Models;
+using ForumAppMVC.Services.Data;
+using ForumAppMVC.Web.ViewModels.Comments;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ForumAppMVC.Web.Controllers
 {
     public class CommentsController : Controller
     {
-        public async Task<IActionResult> Create(CreateCommentInputModel input)
-        {
+        private readonly ICommentsService commentsService;
+        private readonly UserManager<ApplicationUser> userManager;
 
+        public CommentsController(
+            ICommentsService commentsService,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.commentsService = commentsService;
+            this.userManager = userManager;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCommentInputModel input)
+        {
+            var userId = this.userManager.GetUserId(this.User);
+            await this.commentsService.Create(input.PostId, userId, input.Content, parentId);
+            return this.RedirectToAction("ById", "Posts", new { id = input.PostId });
+        }
     }
 }
