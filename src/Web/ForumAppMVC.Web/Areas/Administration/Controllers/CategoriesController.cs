405248df22659fafae384bc92b217760350ee97f
ZackoYo <zarko.y@gmail.com>
2025-05-19 09:09:14 +0300

    Add Categories Controller and Index View

---
+++ src/Web/ForumAppMVC.Web/Areas/Administration/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using ForumAppMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ForumAppMVC.Web.Areas.Administration.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Categories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+    }
+}
