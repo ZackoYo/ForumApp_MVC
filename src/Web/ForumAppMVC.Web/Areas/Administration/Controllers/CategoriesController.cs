82b4d891140a29fb32a16c7173a3604f5ecaa0f2
ZackoYo <zarko.y@gmail.com>
2025-05-23 13:58:23 +0300

    Add Delete Category endpoint

--- src/Web/ForumAppMVC.Web/Areas/Administration/Controllers/CategoriesController.cs
+++ src/Web/ForumAppMVC.Web/Areas/Administration/Controllers/CategoriesController.cs
@@ -108,6 +108,35 @@ namespace ForumAppMVC.Web.Areas.Administration.Controllers
             return View(category);
         }
 
+        // GET: Admin/Categories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Admin/Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
         private bool CategoryExists(int id)
         {
             return _context.Categories.Any(e => e.Id == id);
